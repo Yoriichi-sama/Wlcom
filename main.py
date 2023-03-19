b36be2890cb7516dfb4fea430373139e868d259d
Zomroo <93994060+Zomroo@users.noreply.github.com>
2023-03-20 04:41:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 # Define the font for the welcome message
 font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSerif.ttf', 40)
 
-def welcome(client, message):
+async def welcome(client, message):
     user = message.new_chat_members[0]
     name = user.first_name
     username = user.username
@@ -24,11 +24,11 @@ def welcome(client, message):
     group_name = message.chat.title # get the name of the group where the bot is added
 
     # Get the user's profile picture
-    profile_photos = app.send(GetUserPhotos(user_id=user_id, offset=0, max_id=0, limit=1))
-    if not profile_photos.photos:
-        profile_pic_url = None
+    profile_photos = await app.get_user_profile_photos(user_id=user_id, limit=1)
+    if profile_photos.total_count > 0:
+        profile_pic_url = profile_photos.photos[0][-1].file_id
     else:
-        profile_pic_url = client.get_download_location(profile_photos.photos[0]).download_url
+        profile_pic_url = None
 
     # Download the welcome image
     image_url = 'https://i.postimg.cc/Hsggt1hn/photo-2023-03-20-01-40-46-7212352388177380352.png'
@@ -36,21 +36,33 @@ def welcome(client, message):
     with Image.open(requests.get(image_url, stream=True).raw) as image:
         # Open the image and add the text
         draw = ImageDraw.Draw(image)
-        if profile_pic_url:
-            profile_pic = Image.open(requests.get(profile_pic_url, stream=True).raw).convert("RGBA").resize((200, 200))
-            image.paste(profile_pic, (600, 200), profile_pic)
         draw.text((100, 100), f'Welcome {name}!', fill='white', font=font)
         draw.text((100, 200), f'Username: {username}', fill='white', font=font)
         draw.text((100, 300), f'ID: {user_id}', fill='white', font=font)
         draw.text((100, 400), f'Greetings from {group_name}!', fill='white', font=font) # add the group name to the welcome message
+
+        # Add the user's profile picture to the welcome image
+        if profile_pic_url is not None:
+            # Download the profile picture
+            response = requests.get(profile_pic_url)
+            with Image.open(requests.get(profile_pic_url, stream=True).raw) as profile_pic:
+                # Resize the profile picture to a small circular image
+                size = (150, 150)
+                mask = Image.new('L', size, 0)
+                draw_mask = ImageDraw.Draw(mask) 
+                draw_mask.ellipse((0, 0) + size, fill=255)
+                profile_pic = profile_pic.resize(size)
+                profile_pic.putalpha(mask)
+
+                # Add the profile picture to the welcome image
+                image.paste(profile_pic, (800, 100))
 
         # Save the modified image
         image.save('welcome_modified.jpg')
 
         # Send the modified image as a reply to the welcome message
         with open('welcome_modified.jpg', 'rb') as f:
-            client.send_photo(chat_id=message.chat.id, photo=f, caption=f'Hello {name}! Welcome to the group.')
-
+            await client.send_photo(chat_id=message.chat.id, photo=f, caption=f'Hello {name}! Welcome to the group.')
 
 @app.on_message(filters.new_chat_members)
 def handle_new_chat_members(client, message):
