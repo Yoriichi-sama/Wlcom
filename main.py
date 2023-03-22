fb9a94d715978b8f44933a7b362e9328be879abe
Zomroo <93994060+Zomroo@users.noreply.github.com>
2023-03-22 23:51:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,64 +1,52 @@
-import os
-from io import BytesIO
-from PIL import Image, ImageDraw, ImageFont
 from pyrogram import Client, filters
-from pyrogram.types import Message, User, Photo
+from pyrogram.types import User
+from PIL import Image, ImageDraw, ImageFont
+import requests
+from io import BytesIO
 
-# Set your API hash, API ID, and bot token here
+# Set your API credentials
 api_id = 15849735
-api_hash = 'b8105dc4c17419dfd4165ecf1d0bc100'
+api_hash = 'Yb8105dc4c17419dfd4165ecf1d0bc100'
 bot_token = '6145559264:AAEkUH_znhpaTdkbnndwP1Vy2ppv-C9Zf4o'
 
-# Initialize the client
-app = Client('my_bot', api_id, api_hash, bot_token=bot_token)
+# Create a new Pyrogram client
+bot = Client("my_bot", api_id, api_hash, bot_token=bot_token)
 
-# Define the welcome message and the default profile picture URL
-WELCOME_MESSAGE = "Welcome to the group, {}!"
-DEFAULT_PROFILE_PICTURE_URL = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png"
-
-# Define the font and font size for the user ID
-USER_ID_FONT = ImageFont.truetype("arial.ttf", 12)
-
-# Define the radius for the circle crop
-CROP_RADIUS = 50
-
-# Define the crop function for the user profile picture
-def crop_circle(image):
-    mask = Image.new("L", image.size, 0)
+# Function to generate the welcome message
+async def welcome_message(user: User) -> str:
+    # Get the user's profile picture and convert it to a round shape
+    photo_url = user.photo.big_file_id if user.photo else "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png"
+    photo_binary = BytesIO(requests.get(photo_url).content)
+    profile_pic = Image.open(photo_binary)
+    mask = Image.new("L", profile_pic.size, 0)
     draw = ImageDraw.Draw(mask)
-    draw.ellipse((0, 0, image.size[0], image.size[1]), fill=255)
-    result = Image.new(image.mode, image.size, (255, 255, 255, 0))
-    result.paste(image, (0, 0), mask=mask)
-    return result
+    draw.ellipse((0, 0) + profile_pic.size, fill=255)
+    profile_pic.putalpha(mask)
 
-# Define the function that handles new members joining the group
-@app.on_message(filters.new_chat_members)
-async def welcome(bot, message):
-    for member in message.new_chat_members:
-        # Get the user's profile picture and name
-        profile_pic_url = member.photo.big_file_id if member.photo else DEFAULT_PROFILE_PICTURE_URL
-        name = member.first_name + " " + member.last_name if member.last_name else member.first_name
+    # Generate the welcome message with user's name and id
+    message = f"Welcome {user.mention}!\n"
+    message += f"Name: <b>{user.first_name} {user.last_name}</b>\n"
+    message += f"ID: <code>{user.id}</code>"
 
-        # Load the profile picture and crop it to a circle
-        async with app.session.get(profile_pic_url) as response:
-            profile_pic_data = await response.read()
-        profile_pic = Image.open(BytesIO(profile_pic_data))
-        profile_pic = profile_pic.resize((2*CROP_RADIUS, 2*CROP_RADIUS), resample=Image.BOX)
-        profile_pic = crop_circle(profile_pic)
+    # Create a new image with the welcome message and profile picture
+    canvas = Image.new("RGBA", (800, 200), (255, 255, 255, 255))
+    canvas.paste(profile_pic, (0, 0), profile_pic)
+    font = ImageFont.truetype("arial.ttf", 30)
+    draw = ImageDraw.Draw(canvas)
+    draw.text((200, 40), message, font=font, fill=(0, 0, 0, 255))
 
-        # Create the background image and draw the user's name and ID
-        background = Image.new('RGBA', (400, 150), (255, 255, 255, 255))
-        draw = ImageDraw.Draw(background)
-        draw.text((30, 40), name.upper(), font=ImageFont.truetype("arialbd.ttf", 24), fill=(0, 0, 0, 255))
-        draw.text((30, 80), "ID: {}".format(member.id), font=USER_ID_FONT, fill=(0, 0, 0, 255))
+    # Save the image to a file
+    canvas.save("welcome.png")
 
-        # Paste the profile picture and save the image to a buffer
-        background.paste(profile_pic, (250, 0), profile_pic)
-        buffer = BytesIO()
-        background.save(buffer, format='PNG')
+    # Return the welcome message
+    return message
 
-        # Send the welcome message with the image
-        await message.reply_photo(photo=buffer.getvalue(), caption=WELCOME_MESSAGE.format(member.mention))
+# Handler for new member joined event
+@bot.on_message(filters.new_chat_members)
+async def welcome_new_members(client, message):
+    for user in message.new_chat_members:
+        # Generate the welcome message and send it with the image
+        await message.reply_photo(await client.download_media(user.photo.big_file_id) if user.photo else "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png", caption=await welcome_message(user), parse_mode="HTML")
 
-# Start the client
-app.run()
+# Start the bot
+bot.run()
