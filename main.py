afe13d309cf2d4ccd13766ee3f81f8cb3adae5eb
Zomroo <93994060+Zomroo@users.noreply.github.com>
2023-03-27 00:56:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ async def new_member_handler(client, message):
     font = ImageFont.truetype("/home/gokuinstu2/Wlcom/font.otf", 24)
     draw = ImageDraw.Draw(image)
     text = user_first_name[:20] if len(user_first_name) > 20 else user_first_name
-    draw.text((100, 120), text, font=font, fill=(0, 0, 0))
+    draw.text((100, 170), user_text, font=font, fill=(255, 255, 255))  # white text
 
     # Add user name and ID/username to the left half of the image with font size 14
     font = ImageFont.truetype("font.otf", 14)
