9a3be362e5c362a54b69130bc827d826568286ed
Zomroo <93994060+Zomroo@users.noreply.github.com>
2023-03-26 21:04:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,99 +1,62 @@
-from pyrogram import Client, filters
-from pyrogram.types import Message
+import os
 import requests
 from PIL import Image, ImageDraw, ImageFont
-import io
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-# Initialize your Pyrogram client with your API ID, API hash, and bot token
-app = Client(
-    "my_bot_token",
-    api_id=16844842,
-    api_hash="f6b0ceec5535804be7a56ac71d08a5d4",
-    bot_token="6145559264:AAFufTIozcyIRZPf9bRWCvky2_NhbbjWTKU",
-)
+# Initialize the Telegram bot with your bot token
+updater = Updater(token='6145559264:AAFufTIozcyIRZPf9bRWCvky2_NhbbjWTKU', use_context=True)
+dispatcher = updater.dispatcher
 
-# Define the welcome message template image URL
-TEMPLATE_IMAGE_URL = "https://cdn.pixabay.com/photo/2023/03/22/10/11/lake-7869102_960_720.jpg"
-# Define the default user profile image URL
-DEFAULT_PFP_URL = "https://cdn.pixabay.com/photo/2023/03/22/10/11/lake-7869102_960_720.jpg"
+# Define a function to create the image
+def create_image(update, context):
+    user_id = update.effective_user.id
+    user_name = update.effective_user.name
+    chat_id = update.effective_chat.id
+    
+    # Get the user's profile picture and save it to a file
+    file_path = f'{user_id}.jpg'
+    url = context.bot.get_user_profile_photos(user_id).photos[0][-1].file_id
+    file = context.bot.get_file(url)
+    file.download(file_path)
+    
+    # Open the background image and resize it to 800x600
+    background_image = Image.open('https://i.postimg.cc/MHz17VTg/Screenshot-73.png')
+    background_image = background_image.resize((800, 600))
+    
+    # Open the user's profile picture and resize it to 200x200
+    profile_picture = Image.open(file_path)
+    profile_picture = profile_picture.resize((200, 200))
+    
+    # Create a circular mask for the profile picture
+    mask = Image.new('L', (200, 200), 0)
+    draw = ImageDraw.Draw(mask)
+    draw.ellipse((0, 0, 200, 200), fill=255)
+    
+    # Paste the profile picture onto the background image using the mask
+    background_image.paste(profile_picture, (550, 200), mask=mask)
+    
+    # Add a text message to the image
+    font = ImageFont.truetype('arial.ttf', 36)
+    draw = ImageDraw.Draw(background_image)
+    draw.text((50, 50), f'{user_name} has joined the group!', fill='black', font=font)
+    
+    # Save the image to a file
+    image_path = f'{user_id}_image.jpg'
+    background_image.save(image_path)
+    
+    # Send the image to the group chat
+    context.bot.send_photo(chat_id=chat_id, photo=open(image_path, 'rb'))
+    
+    # Delete the user's profile picture file and image file
+    os.remove(file_path)
+    os.remove(image_path)
 
-# Define the font style and size for the text
-TEXT_FONT = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf", 50)
+# Define a function to handle new user joins
+def new_member(update, context):
+    create_image(update, context)
 
-# Define the circle size and border color for the user profile image
-PFP_SIZE = (150, 150)
-PFP_BORDER_COLOR = (255, 255, 255)
+# Add a handler for new user joins
+dispatcher.add_handler(MessageHandler(Filters.status_update.new_chat_members, new_member))
 
-# Define the welcome message text
-WELCOME_MESSAGE = "Welcome to the chat, {}!"
-
-# Create a function to generate the welcome image
-async def generate_welcome_image(name: str, user_id: int, pfp_url: str) -> io.BytesIO:
-    # Open the template image from the URL
-    template_image = Image.open(requests.get(TEMPLATE_IMAGE_URL, stream=True).raw)
-
-    # Create a new ImageDraw object
-    draw = ImageDraw.Draw(template_image)
-
-    # Draw the user's name in the top-left corner in large bond letters
-    draw.text((100, 80), name.upper(), font=TEXT_FONT, fill=(255, 255, 255))
-
-    # Draw the "Welcome to the chat" text below the name in small itely letters
-    draw.text((100, 200), WELCOME_MESSAGE.format(name), font=TEXT_FONT, fill=(255, 255, 255))
-
-    # Get the user's profile picture URL
-    if pfp_url:
-        response = requests.get(pfp_url)
-        if response.status_code == 200:
-            pfp_image = Image.open(io.BytesIO(response.content))
-        else:
-            pfp_image = Image.open(requests.get(DEFAULT_PFP_URL, stream=True).raw)
-    else:
-        pfp_image = Image.open(requests.get(DEFAULT_PFP_URL, stream=True).raw)
-
-    # Resize the profile image to a circle shape and add a border
-    pfp_image = pfp_image.resize(PFP_SIZE)
-    pfp_mask = Image.new("L", PFP_SIZE, 0)
-    ImageDraw.Draw(pfp_mask).ellipse((0, 0) + PFP_SIZE, fill=255)
-    pfp_image.putalpha(pfp_mask)
-    pfp_image_border = Image.new("RGBA", PFP_SIZE, PFP_BORDER_COLOR + (0,))
-    ImageDraw.Draw(pfp_image_border).ellipse((0, 0) + PFP_SIZE, outline=PFP_BORDER_COLOR, width=5)
-    pfp_image_border.alpha_composite(pfp_image)
-
-    # Paste the profile image and user ID on the right side of the template image
-    template_image.paste(pfp_image_border, (960, 240))
-    draw.text((1050, 420), f"ID: {user_id}", font=TEXT_FONT, fill=(255, 255, 255))
-
-    # Convert the image to bytes and return the result
-    result = io.BytesIO()
-    template_image.save(result, format="JPEG")
-    result.seek(0)
-    return result
-
-# Define a function to handle new members joining a chat
-@app.on_message(filters.group & filters.new_chat_members)
-async def handle_new_chat_members(client: Client, message: Message):
-    # Iterate over the new members in the message
-    for member in message.new_chat_members:
-        # Get the member's name, ID, and profile image URL (if available)
-        name = member.first_name + " " + member.last_name if member.last_name else member.first_name
-        user_id = member.id
-        pfp_url = None
-        if member.photo:
-            pfp_url = member.photo.big_file_id
-
-        # Generate the welcome image
-        image_bytes = generate_welcome_image(name, user_id, pfp_url)
-
-        # Reply to the new member with the welcome message and image
-        await message.reply_photo(image_bytes, caption=WELCOME_MESSAGE.format(f"@{member.username}" if member.username else name))
-
-
-# Define a function to handle the "/start" command in private messages
-@app.on_message(filters.private & filters.command("start"))
-async def handle_start_command(client: Client, message: Message):
-    # Reply to the user with a welcome message
-    await message.reply_text("Hi there! I'm a bot that welcomes new members to groups.")
-
-# Run the bot
-app.run()
+# Start the bot
+updater.start_polling()
