67f6f6d0af426d9ff220f285e2c13c296e4f91b9
Zomroo <93994060+Zomroo@users.noreply.github.com>
2023-03-27 00:33:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,73 +1,62 @@
-import logging
-import os
-from io import BytesIO
+from pyrogram import Client, filters
+from pyrogram.types import InlineQuery, InlineQueryResultPhoto, User
+from PIL import Image, ImageDraw, ImageFont
+import io
+from Bot.plugins.help import HELP_STRINGS
+from Bot.database.sudos import SudosDatabase
 
-import requests
-from PIL import Image, ImageDraw
-from telegram import Update
-from telegram.ext import CallbackContext, CommandHandler, Filters, MessageHandler, Updater
-from telegram import User
+# Bot API credentials
+api_id = 16844842
+api_hash = 'f6b0ceec5535804be7a56ac71d08a5d4'
+bot_token = '6145559264:AAFufTIozcyIRZPf9bRWCvky2_NhbbjWTKU'
 
-# Define your bot token and logging setup
-BOT_TOKEN = "6145559264:AAFufTIozcyIRZPf9bRWCvky2_NhbbjWTKU"
-logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
-)
+# Create Pyrogram client and start bot
+bot = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
+# Start the bot and define a handler for new users joining the group
+@bot.on_message(filters.new_chat_members)
+async def new_member_handler(client, message):
+    # Get the user who just joined the group
+    user = message.new_chat_members[0]
 
-# Define a function to create a circular mask for the profile picture
-def circle_mask(size):
-    mask = Image.new("RGBA", (size, size), (0, 0, 0, 0))
+    # Get user details
+    user_id = user.id
+    user_name = user.username
+    user_pic = user.photo.big_file_id
+    user_first_name = user.first_name
+
+    # Load user profile picture and resize to 200x200
+    photo_bytes = await client.download_media(user_pic, file_name='process.png', in_memory=True)
+    photo_bytes = bytes(photo_bytes.getbuffer())        
+    photo = Image.open(io.BytesIO(photo_bytes)).resize((200, 200))
+
+    # Create new image with size 700x300 and white background
+    image = Image.new('RGB', (700, 300), (255, 255, 255))
+
+    # Create circular mask
+    mask = Image.new('L', photo.size, 0)
     draw = ImageDraw.Draw(mask)
-    draw.ellipse((0, 0, size, size), fill=(255, 255, 255, 255))
-    return mask
+    draw.ellipse((0, 0) + photo.size, fill=255)
 
+    # Paste user profile picture on the right side of the image with the circular mask
+    image.paste(photo, (500, 50), mask=mask)
 
-# Define a function to create the welcome message image
-def send_welcome_message(update: Update, context: CallbackContext):
-    # Get the new user's profile picture
-    user: User = update.effective_user
-    profile_pic_file = context.bot.get_user_profile_photos(user.id).photos[0][-1].get_file()
-    profile_pic_bytes = BytesIO()
-    profile_pic_file.download(out=profile_pic_bytes)
+    # Add user name to the left half of the image with font size 24 and text limit of 20
+    font = ImageFont.truetype("font.otf", 24)
+    draw = ImageDraw.Draw(image)
+    text = user_first_name[:20] if len(user_first_name) > 20 else user_first_name
+    draw.text((100, 120), text, font=font, fill=(0, 0, 0))
 
-    # Open the welcome image template
-    welcome_image_url = "https://i.imgur.com/uNHJ1eZ.jpg"
-    welcome_image_bytes = BytesIO(requests.get(welcome_image_url).content)
-    welcome_image = Image.open(welcome_image_bytes)
+    # Add user name and ID/username to the left half of the image with font size 14
+    font = ImageFont.truetype("font.otf", 14)
+    user_text = f"{user_name}\n(ID: {user_id})" if user_name else f"ID: {user_id}"
+    draw.text((100, 170), user_text, font=font, fill=(0, 0, 0))
 
-    # Crop the profile picture to a square
-    profile_pic = Image.open(profile_pic_bytes)
-    profile_pic = profile_pic.convert("RGBA")
-    size = min(profile_pic.width, profile_pic.height)
-    profile_pic = profile_pic.crop(((profile_pic.width - size) // 2, (profile_pic.height - size) // 2, (profile_pic.width + size) // 2, (profile_pic.height + size) // 2))
-
-    # Create a circular mask for the profile picture
-    mask = circle_mask(size)
-    profile_pic.putalpha(mask.split()[-1])
-
-    # Paste the profile picture onto the welcome image
-    frame_size = (354, 354)
-    frame = Image.new("RGBA", frame_size, (0, 0, 0, 0))
-    profile_pic.thumbnail((frame_size[0]-20, frame_size[1]-20), Image.LANCZOS)
-    frame.paste(profile_pic, (10, 10), profile_pic)
-
-    # Paste the frame onto the welcome image
-    welcome_image.paste(frame, (580, 95), frame)
-
-    # Save the final image and send it to the user
-    welcome_image_bytes = BytesIO()
-    welcome_image.save(welcome_image_bytes, format="JPEG")
-    welcome_image_bytes.seek(0)
-    context.bot.send_photo(chat_id=update.message.chat_id, photo=welcome_image_bytes, caption=f"Welcome to the group, {user.first_name}!")
-
-
-# Create the bot and add the necessary handlers
-updater = Updater(BOT_TOKEN, use_context=True)
-dispatcher = updater.dispatcher
-dispatcher.add_handler(MessageHandler(Filters.status_update.new_chat_members, send_welcome_message))
-
+    # Save image to a byte stream and send as photo to the group
+    with io.BytesIO() as bio:
+        image.save(bio, "PNG")
+        bio.seek(0)
+        await client.send_photo(chat_id=message.chat.id, photo=bio)
 
 # Start the bot
-updater.start_polling()
-updater.idle()
+bot.run()
