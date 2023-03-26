e337e0c8621c237331acc552548eacf549da8847
Zomroo <93994060+Zomroo@users.noreply.github.com>
2023-03-27 00:13:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,45 +12,39 @@ from telegram import Update
 BOT_TOKEN = "6145559264:AAFufTIozcyIRZPf9bRWCvky2_NhbbjWTKU"
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-# Define the function that will send the welcome message and image
-def send_welcome_message(update: Update, context: CallbackContext) -> None:
-    # Get the user's profile picture
-    user_profile_pic = context.bot.get_user_profile_photos(update.message.from_user.id).photos[0][-1]
-    file = context.bot.get_file(user_profile_pic.file_id)
-    file_bytes = BytesIO(file.download_as_bytearray())
-    profile_pic = Image.open(file_bytes).convert('RGBA')
+# Define a function to create a circular mask for the profile picture
+def send_welcome_message(update: Update, context: CallbackContext):
+    # Get the new user's profile picture
+    user: User = update.effective_user
+    profile_pic_file = context.bot.get_user_profile_photos(user.id).photos[0][-1].get_file()
+    profile_pic_bytes = BytesIO()
+    profile_pic_file.download(out=profile_pic_bytes)
     
-    # Load the welcome image template and create a transparent layer
+    # Create a circular profile picture with the correct dimensions
+    profile_pic = Image.open(profile_pic_bytes)
+    profile_pic = profile_pic.convert("RGBA")
+    size = (min(profile_pic.width, profile_pic.height),) * 2  # Use the minimum dimension as the size of the square
+    mask = circle_mask(size[0])
+    profile_pic.putalpha(mask.split()[-1])
+    profile_pic.thumbnail((500, 500))
+
+    # Load the welcome image template and draw the user's profile picture
     welcome_image_url = "https://graph.org/file/b86f6ed0d2634be5def3d.jpg"
     welcome_image_bytes = BytesIO(requests.get(welcome_image_url).content)
-    welcome_image = Image.open(welcome_image_bytes).convert('RGBA')
-    alpha_layer = Image.new('RGBA', welcome_image.size, (0, 0, 0, 0))
-    
-    # Resize and crop the user's profile picture to fit the frame
-    size = (150, 150)
-    profile_pic.thumbnail(size, Image.ANTIALIAS)
-    x_offset = 670
-    y_offset = 120
-    border_width = 4
-    border = Image.new('RGBA', (size[0] + border_width * 2, size[1] + border_width * 2), (255, 255, 255, 255))
-    border_draw = ImageDraw.Draw(border)
-    border_draw.rectangle((0, 0, size[0] + border_width * 2 - 1, size[1] + border_width * 2 - 1), outline=(0, 0, 0, 255), width=border_width)
-    alpha_layer.paste(border, (x_offset - border_width, y_offset - border_width))
-    alpha_layer.paste(profile_pic, (x_offset, y_offset), profile_pic)
+    welcome_image = Image.open(welcome_image_bytes)
+    welcome_image.paste(profile_pic, (670, 120))
 
-    # Merge the welcome image and the alpha layer
-    welcome_image = Image.alpha_composite(welcome_image, alpha_layer)
-    
-    # Save the welcome image as bytes and send it to the user
+    # Save the final image and send it to the user
     welcome_image_bytes = BytesIO()
-    welcome_image.convert('RGB').save(welcome_image_bytes, format='JPEG')
-    context.bot.send_photo(update.message.chat_id, photo=welcome_image_bytes.getvalue(), caption="Welcome!")
+    welcome_image.save(welcome_image_bytes, format='JPEG')
+    welcome_image_bytes.seek(0)
+    context.bot.send_photo(chat_id=update.message.chat_id, photo=welcome_image_bytes, caption=f"Welcome to the group, {user.first_name}!")
 
-# Define a function to create a circular mask for the profile picture    
-def circle_mask(size):    
-    mask = Image.new("RGBA", (size, size), (0, 0, 0, 0))    
-    draw = ImageDraw.Draw(mask)    
-    draw.ellipse((0, 0, size, size), fill=(255, 255, 255, 255))    
+# Define a function to create a circular mask for the profile picture
+def circle_mask(size):
+    mask = Image.new("RGBA", (size, size), (0, 0, 0, 0))
+    draw = ImageDraw.Draw(mask)
+    draw.ellipse((0, 0, size, size), fill=(255, 255, 255, 255))
     return mask
 
 
