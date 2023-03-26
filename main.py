7abba07dc9373ee607708628170b507a5c2083b6
Zomroo <93994060+Zomroo@users.noreply.github.com>
2023-03-27 02:34:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ async def new_member_handler(client, message):
     # Add group name below the user's name
     group_name = message.chat.title
     font = ImageFont.truetype("/home/gokuinstu2/Wlcom/font.otf", 14)
-    text = f"Welcome to: {group_name[:14]}"
+    text = f"Welcome to : {group_name[:14]}"
     draw.text((110, 160), text, font=font, fill=(255, 255, 255))
     if len(group_name) > 14:
         text = group_name[14:28]
